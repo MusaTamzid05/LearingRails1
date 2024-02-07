37e08d80684314a4e8523e68e161b587b8368856
MusaTamzid05 <tamzid101@gmail.com>
2024-02-07 17:17:38 +0600

    Create Show for main gallery

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,4 +2,9 @@ class MainController < ApplicationController
   def index
     @photos =  Photo.all
   end
+
+  def show
+    puts "calling show"
+    @photo = Photo.find(params[:photo_id])
+  end
 end
